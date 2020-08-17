b2bf5325f8e37534ecfe1240015d7431a4c7f373
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-08-17 09:48:20 -0700

    adds ability to reorder list

--- src/index.js
+++ src/index.js
@@ -9,7 +9,28 @@ class App extends React.Component {
   state = initialData;
 
   onDragEnd = result => {
-    //TODO: reorder the column
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId && 
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 1, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
   }
 
   render() {
